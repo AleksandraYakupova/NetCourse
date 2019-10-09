1e44c77398283325800e96b4054561ac7af1180a
AleksandraYakupova <maxlex047@gmail.com>
2019-10-09 12:37:00 +0300

    Проекты для 3й лабы

--- Net_Lab1_Projects/Net_Lab1/main.cpp
+++ Net_Lab1_Projects/Net_Lab1/main.cpp
@@ -1,9 +1,12 @@
 #include <server.h>
 #include <QApplication>
+#include <QString>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QString str("2e+308");
+    double d = str.toDouble();
     Server server(2424);
     server.show();
     return a.exec();
